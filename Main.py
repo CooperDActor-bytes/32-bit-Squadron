e882ff5771992ddcd78c8f5ff70cca3effa8a3ac
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2024-12-16 21:09:32 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,90 +1,77 @@
 import pygame,random
 
-#x and y values
-x = 720
-y = 720
-px = 310
-py = 620
-cx1 = random.randint(0,640)
-cy1 = random.randint(-1000,0)
-cx2 = random.randint(0,640)
-cy2 = random.randint(-1000,0)
-cx3 = random.randint(0,640)
-cy3 = random.randint(-1000,0)
-tx1 = random.randint(0,640)
-ty1 = random.randint(-1000,0)
-tx2 = random.randint(0,640)
-ty2 = random.randint(-1000,0)
-lx = random.randint(0,640)
-ly = random.randint(-1000,0)
-ex1 = random.randint(0,640)
-ey1 = random.randint(-1000,0)
-ex2 = random.randint(0,640)
-ey2 = random.randint(-1000,0)
-ex3 = random.randint(0,640)
-ey3 = random.randint(-1000,0)
-ex4 = random.randint(0,640)
-ey4 = random.randint(-1000,0)
-ex5 = random.randint(0,640)
-ey5 = random.randint(-1000,0)
-e1bmbx = ex1 + 35
-e1bmby = ey1
-e3bmbx = ex3 + 35
-e3bmby = ey3
-e5bmbx = ex5 + 35
-e5bmby = ey5
-bx = random.randint(0,640)
-by = random.randint(-1000,0)
-prx = random.randint(0,640)
-pry = random.randint(-1000,0)
-
-#values
-spvel = 10
-vel = 5
-screen = pygame.display.set_mode((x,y))
-run = True
-page = "menu"
-clock = pygame.time.Clock()
-keys = pygame.key.get_pressed()
-
 pygame.init()
 pygame.display.set_icon(pygame.image.load("Assets/32bit Squadron logo.png"))
 pygame.display.set_caption("32-bit Squadron") 
 
+screen = pygame.display.set_mode((1280,720),pygame.RESIZABLE)
+run = True
+page = "menu"
+pts = 0
+frametime = 0
+clock = pygame.time.Clock()
+keys = pygame.key.get_pressed()
+
+def text(text,font,txtcol,txx,txy):
+    txtsprite = font.render(text,True,txtcol)
+    screen.blit(txtsprite,(txx,txy))
+
 while run:
     clock.tick(60)
+    x,y = screen.get_width(),screen.get_height()
+    spvel = y / 720 * 15
+    vel = y / 720 * 10
 
     if page == "menu":
-        #resets variables
-        px = 310
-        ex1 = random.randint(0,640)
-        ey1 = random.randint(-1000,0)
-        ex2 = random.randint(0,640)
-        ey2 = random.randint(-1000,0)
-        ex3 = random.randint(0,640)
-        ey3 = random.randint(-1000,0)
-        ex4 = random.randint(0,640)
-        ey4 = random.randint(-1000,0)
-        ex5 = random.randint(0,640)
-        ey5 = random.randint(-1000,0)
-        e1bmbx = ex1 + 35
-        e1bmby = ey1
-        e3bmbx = ex3 + 35
-        e3bmby = ey3
-        e5bmbx = ex5 + 35
-        e5bmby = ey5
+        screen.fill((0,50,0))
+        pygame.draw.rect(screen,(70,70,70),pygame.Rect(0,0,700,y))
+        pygame.draw.rect(screen,(80,80,80),pygame.Rect(0,0,700,y-150))
+        gamestart = screen.blit(pygame.image.load("Assets/Start.png"),(100,y / 2 - 125))
+        gamecredits = screen.blit(pygame.image.load("Assets/Credits.png"),(100,y / 2 + 10))
+        gamequit = screen.blit(pygame.image.load("Assets/Quit.png"),(362,y / 2 + 10))
+        screen.blit(pygame.image.load("Assets/32bit Squadron logo.png"),((x - 300) / 2 + 300,y / 2 - 250))
+        text("Points:",pygame.font.SysFont("Roboto",100),(255,255,255),100,y-100)
+        text(str(pts),pygame.font.SysFont("Roboto",100),(255,255,255),362,y-100)
 
-        #renders menu
-        screen.fill((70,70,70))
-        screen.blit(pygame.image.load("Assets/32bit Squadron logo.png"),(160,30))
-        gamestart = screen.blit(pygame.image.load("Assets/Start.png"),(120,450))
-        gamecredits = screen.blit(pygame.image.load("Assets/Credits.png"),(120,575))
-        gamequit = screen.blit(pygame.image.load("Assets/Quit.png"),(380,575))
-
-        #detects events
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if gamestart.collidepoint(event.pos):
+                    px = x / 2 - 40
+                    py = y - 100
+                    cx1 = random.randint(0,x-80)
+                    cy1 = random.randint(-1000,0)
+                    cx2 = random.randint(0,x-80)
+                    cy2 = random.randint(-1000,0)
+                    cx3 = random.randint(0,x-80)
+                    cy3 = random.randint(-1000,0)
+                    tx1 = random.randint(0,x-80)
+                    ty1 = random.randint(-1000,0)
+                    tx2 = random.randint(0,x-80)
+                    ty2 = random.randint(-1000,0)
+                    lx = random.randint(0,x-80)
+                    ly = random.randint(-1000,0)
+                    ex1 = random.randint(0,x-80)
+                    ey1 = random.randint(-1000,0)
+                    ex2 = random.randint(0,1000)
+                    ey2 = random.randint(-1000,0)
+                    ex3 = random.randint(0,x-80)
+                    ey3 = random.randint(-1000,0)
+                    ex4 = random.randint(0,x-80)
+                    ey4 = random.randint(-1000,0)
+                    ex5 = random.randint(0,x-80)
+                    ey5 = random.randint(-1000,0)
+                    e1bmbx = ex1 + 35
+                    e1bmby = ey1
+                    e3bmbx = ex3 + 35
+                    e3bmby = ey3
+                    e5bmbx = ex5 + 35
+                    e5bmby = ey5
+                    bx = random.randint(0,x-80)
+                    by = random.randint(-1000,0)
+                    prx = random.randint(0,x-80)
+                    pry = random.randint(-1000,0)
+                    frametime = 0
+                    pts = 0
                     page = "game"
                 if gamecredits.collidepoint(event.pos):
                     page = "credits"
@@ -94,11 +81,11 @@ while run:
             run = False
 
     if page == "credits":
-        screen.fill((70,70,70))
-        screen.blit(pygame.image.load("Assets/Credits Scene.png"),(50,100))
-        screen.blit(pygame.image.load("Assets/32bit Squadron logo.png"),(300,40))
-        screen.blit(pygame.image.load("Assets/Game Screenshot.png"),(50,450))
-        gamemenu = screen.blit(pygame.image.load("Assets/Main menu.png"),(390,500))
+        screen.fill((0,50,0))
+        pygame.draw.rect(screen,(70,70,70),pygame.Rect(0,0,700,y))
+        screen.blit(pygame.image.load("Assets/32bit Squadron logo.png"),((x - 300) / 2 + 300,y / 2 - 250))
+        screen.blit(pygame.image.load("Assets/Credits Scene.png"),((100),y / 2 - 282))
+        gamemenu = screen.blit(pygame.image.load("Assets/Main menu.png"),(100,y / 2 + 50))
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if gamemenu.collidepoint(event.pos):
@@ -109,7 +96,6 @@ while run:
     if page == "game":
         screen.fill((0,50,0))
 
-        #renders sprites
         screen.blit(pygame.image.load("Assets/Lake.png"),(lx,ly))
         screen.blit(pygame.image.load("Assets/Tree.png"),(tx1,ty1))
         screen.blit(pygame.image.load("Assets/Tree.png"),(tx2,ty2))
@@ -127,126 +113,130 @@ while run:
         screen.blit(pygame.image.load("Assets/Cloud 1.png"),(cx1,cy1))
         screen.blit(pygame.image.load("Assets/Cloud 2.png"),(cx2,cy2))
         screen.blit(pygame.image.load("Assets/Cloud 3.png"),(cx3,cy3))
+        text(str(pts),pygame.font.SysFont("Roboto",150),(255,255,255),10,10)
 
-        #player movement
+        py = y - 100
+        
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d] and px < 640:
-          px += spvel
+        if keys[pygame.K_d] and px < x-80:
+            px += spvel
         if keys[pygame.K_a] and px > 0:
-          px -= spvel
-        if keys[pygame.K_RIGHT] and px < 640:
-          px += spvel
+            px -= spvel
+        if keys[pygame.K_RIGHT] and px < x-80:
+            px += spvel
         if keys[pygame.K_LEFT] and px > 0:
-          px -= spvel
+            px -= spvel
 
-        #player collision
+        if frametime == 60:
+            frametime = 0
+            pts += 1
+
         if pygame.Rect.colliderect(player,enemy1):
-           page = "menu"
+            screen.fill((0,50,0))
+            page = "menu"
         if pygame.Rect.colliderect(player,e1bomb):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,enemy2):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,enemy3):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,e3bomb):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,enemy4):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,enemy5):
-           page = "menu"
+            page = "menu"
         if pygame.Rect.colliderect(player,e5bomb):
-           page = "menu"
+            page = "menu"
 
-        #cloud movement
-        if cy1 > 720:
-            cx1 = random.randint(0,640)
+        if cx1 > x or cy1 > y:
+            cx1 = random.randint(-80,y)
             cy1 = random.randint(-1000,0)
         else:
-           cy1 += vel
-        if cy2 > 720:
-           cx2 = random.randint(0,640)
-           cy2 = random.randint(-1000,0)
+            cy1 += vel
+            cx1 += 1
+        if cx2 > x or cy2 > y:
+            cx2 = random.randint(-80,y)
+            cy2 = random.randint(-1000,0)
         else:
-           cy2 += vel
-        if cy3 > 720:
-           cx3 = random.randint(0,640)
-           cy3 = random.randint(-1000,0)
+            cy2 += vel
+            cx2 += 1
+        if cx3 > x or cy3 > y:
+            cx3 = random.randint(-80,y)
+            cy3 = random.randint(-1000,0)
         else:
-           cy3 += vel
+            cy3 += vel
+            cx3 += 1
 
-        #tree movement
-        if ty1 > 720:
-            tx1 = random.randint(0,640)
+        if ty1 > y:
+            tx1 = random.randint(0,x-80)
             ty1 = random.randint(-1000,0)
         else:
            ty1 += vel
-        if ty2 > 720:
-            tx2 = random.randint(0,640)
+        if ty2 > y:
+            tx2 = random.randint(0,x-80)
             ty2 = random.randint(-1000,0)
         else:
             ty2 += vel
 
-        #Lake movement
-        if ly > 720:
-            lx = random.randint(0,640)
+        if ly > y:
+            lx = random.randint(0,x-80)
             ly = random.randint(-1000,0)
         else:
             ly += vel
 
-        #Base movement
-        if by > 720:
-            bx = random.randint(0,640)
+        if by > y:
+            bx = random.randint(0,x-80)
             by = random.randint(-1000,0)
         else:
             by += vel
 
-        #parachute movement
-        if pry > 720:
-            prx = random.randint(0,640)
+        if pry > y:
+            prx = random.randint(0,x-80)
             pry = random.randint(-1000,0)
         else:
             pry += vel
 
-        #enemy movement
-        if ey1 > 720:
-            ex1 = random.randint(0,640)
+        if ey1 > y:
+            ex1 = random.randint(0,x-80)
             ey1 = random.randint(-1000,0)
             e1bmbx = ex1 + 34
             e1bmby = ey1 + 50
         else:
             ey1 += spvel
-            e1bmby += 15
-        if ey2 > 720:
-            ex2 = random.randint(0,640)
+            e1bmby += spvel + 2
+        if ey2 > y:
+            ex2 = random.randint(0,x-80)
             ey2 = random.randint(-1000,0)
         else:
             ey2 += spvel
-        if ey3 > 720:
-            ex3 = random.randint(0,640)
+        if ey3 > y:
+            ex3 = random.randint(0,x-80)
             ey3 = random.randint(-1000,0)
             e3bmbx = ex3 + 34
             e3bmby = ey3 + 50
         else:
             ey3 += spvel
-            e3bmby += 15
-        if ey4 > 720:
-            ex4 = random.randint(0,640)
+            e3bmby += spvel + 2
+        if ey4 > y:
+            ex4 = random.randint(0,x-80)
             ey4 = random.randint(-1000,0)
         else:
            ey4 += spvel
-        if ey5 > 720:
-            ex5 = random.randint(0,640)
+        if ey5 > y:
+            ex5 = random.randint(0,x-80)
             ey5 = random.randint(-1000,0)
             e5bmbx = ex5 + 34
             e5bmby = ey5 + 50
         else:
             ey5 += spvel
-            e5bmby += 15
+            e5bmby += spvel + 2
             
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+        frametime += 1
     pygame.display.update()
     
 pygame.quit()
